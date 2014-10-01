d7dd15b2a848f75690950f4b57a71d424c4ad13a
pajarraco <mail@pajarraco.com>
2014-10-01 22:04:38 +1000

    challenge 6

---
+++ program.js
@@ -0,0 +1,15 @@
+
+var module_file = require('./module_file.js')
+
+
+    var dir = process.argv[2]
+    var filterStr = process.argv[3]
+    
+    filterFn(dir, filterStr, function (err, list) {
+      if (err)
+        return console.error('There was an error:', err)
+    
+      list.forEach(function (file) {
+        console.log(file)
+      })
+    })
