91d9d0e950e4c9b499287bec59230817ce3f58bb
pajarraco <mail@pajarraco.com>
2014-10-08 21:41:07 +1000

    node school solutions

--- program.js
+++ program.js
@@ -1,20 +1,7 @@
-var http = require('http')
-
-
-var url = process.argv[2]
-http.get(url, function(res) {
-	res.on("error", function(err) {
-		console.log('error: ', err)
-	})
-
-	res.on("data", function(data) {
-
-		console.log(data.toString())
-		//var arr = data.toString().split('\n')
-		//console.log('arr:', arr)
-		//arr.forEach(function(str) {
-		//	console.log('str', str)
-		//})
-	})
-
-})
+    var http = require('http')
+    
+    http.get(process.argv[2], function (response) {
+      response.setEncoding('utf8')
+      response.on('data', console.log)
+      response.on('error', console.error)
+    })
